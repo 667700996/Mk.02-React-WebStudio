210136d0c39b57f3f328fcc5038fd6f85f19a4c1
667700996 <hwan@onnuripg.com>
2025-12-30 11:38:58 +0900

    -

--- src/components/ui/SmoothScrolling.tsx
+++ src/components/ui/SmoothScrolling.tsx
@@ -1,15 +1,7 @@
-'use client';
-
-import { ReactLenis } from '@studio-freight/react-lenis';
-
 interface SmoothScrollingProps {
   children: React.ReactNode;
 }
 
 export default function SmoothScrolling({ children }: SmoothScrollingProps) {
-  return (
-    <ReactLenis root options={{ duration: 0.6, wheelMultiplier: 1.2, smoothWheel: true }}>
-      <>{children}</>
-    </ReactLenis>
-  );
+  return <>{children}</>;
 }
